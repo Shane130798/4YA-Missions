d09800317276382e4f1ddcfb05bf0270b6d5e58b
jayirving21 <86188742+jayirving21@users.noreply.github.com>
2021-10-29 16:52:40 +0100

    A

---
+++ European Servers/BetaServerEvent/1. Lua Script Files/oc2/4YA_MISSION_A1.lua
@@ -0,0 +1,75 @@
+USAF_1 = SPAWN:New( "USAF_1" )
+:InitRandomizePosition(true,0,0)
+	:InitLimit( 2, 500 )
+	:SpawnScheduled( 50, .100 )
+ 
+USAF_2 = SPAWN:New( "USAF_2" )
+:InitRandomizePosition(true,0,0)
+	:InitLimit( 2, 500 )
+	:SpawnScheduled( 50, .100 )
+ 
+USAF_3 = SPAWN:New( "USAF_3" ) 
+:InitRandomizePosition(true,0,0)
+	:InitLimit( 2, 500 )
+	:SpawnScheduled( 50, .100 )
+ 
+USAF_4 = SPAWN:New( "USAF_4" )
+:InitRandomizePosition(true,0,0)
+	:InitLimit( 2, 500 )
+	:SpawnScheduled( 100, .100 )
+
+USAF_5 = SPAWN:New( "USAF_5" ) 
+:InitRandomizePosition(true,0,0)
+	:InitLimit( 2, 500 )
+	:SpawnScheduled( 100, .100 )
+ 
+USAF_6 = SPAWN:New( "USAF_6" )
+:InitRandomizePosition(true,0,0)
+	:InitLimit( 2, 500 )
+	:SpawnScheduled( 100, .100 )
+	
+CONVOY_7 = SPAWN:New( "CONVOY_7" )
+:InitRandomizePosition(true,0,0)
+	:InitLimit( 6, 500 )
+	:SpawnScheduled( 100, .100 )
+
+CONVOY_8 = SPAWN:New( "CONVOY_8" )
+:InitRandomizePosition(true,0,0)
+	:InitLimit( 6, 980 )
+	:SpawnScheduled( 100, .100 )
+
+CONVOY_9 = SPAWN:New( "CONVOY_9" )
+:InitRandomizePosition(true,0,0)
+	:InitLimit( 7, 500 )
+	:SpawnScheduled( 100, .100 )
+
+CONVOY_10 = SPAWN:New( "CONVOY_10" )
+:InitRandomizePosition(true,0,0)
+	:InitLimit( 6, 500 )
+	:SpawnScheduled( 100, .100 )	
+
+CONVOY_11 = SPAWN:New( "CONVOY_11" )
+:InitRandomizePosition(true,0,0)
+	:InitLimit( 7, 500 )
+	:SpawnScheduled( 100, .100 )
+  
+AWACS_BLUE_OVERLORD = SPAWN:New( "AWACS_BLUE_OVERLORD" ) 
+	:InitLimit( 1, 500 )
+	:SpawnScheduled( 200, .100 )
+	
+AWACS_BLUE_OVERLORD2 = SPAWN:New( "AWACS_BLUE_OVERLORD2" ) 
+	:InitLimit( 1, 500 )
+	:SpawnScheduled( 200, .100 )	
+ 
+TANKERS3B_BLUE = SPAWN:New( "TANKERS3B_BLUE" ) 
+	:InitLimit( 1, 500 )
+	:SpawnScheduled( 780, .870 )
+
+TANKER135_BLUE = SPAWN:New( "TANKER135_BLUE" ) 
+	:InitLimit( 1, 500 )
+	:SpawnScheduled( 780, .870 )
+
+TANKER135_BLUE_MPRS = SPAWN:New( "TANKER135_BLUE_MPRS" ) 
+	:InitLimit( 1, 500 )
+	:SpawnScheduled( 40, .30 )
+
