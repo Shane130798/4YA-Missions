d7fe78dff8bda6cdfd594c88842e0255fcee4448
4yaAdmin <4yadcs@gmail.com>
2021-10-29 16:20:34 -0700

    A

--- American Servers/BetaServer3 4YA5 MARIANAS/Config/serverSettings.lua
+++ American Servers/BetaServer3 4YA5 MARIANAS/Config/serverSettings.lua
@@ -1,62 +1,43 @@
-cfg = 
-{
-    ["description"] = "[4YA] TRAINING 24/7 MARIANAS PVE BETA US SERVER\
-\
-**DISCORD**  https://discord.gg/CaYRRDd\
-**SRS**      Type  -motd  in-game chat for ip\
-\
-Welcome to 4YA Training Grounds mission, this mission is designed to help both new and experienced pilots refine \
-their skills in their chosen airframe. The mission incorporates both fixed wing and rotary wing aircraft and the \
-ability to train on both Air to Air/Air to Ground techniques, missile evasion, land-based circuit landings as \
-well as carrier-based recovery operations. You may fly alone or with a wingman/squadron to develop skills essential in a coordinated attack.\
-\
-Available:\
-\
-Ctld\
-Combined Arms\
-PVE\
-SRS\
-CarrierOps ",
-    ["require_pure_textures"] = false,
-    ["listStartIndex"] = 2,
-    ["advanced"] = 
-    {
-        ["allow_change_tailno"] = true,
-        ["disable_events"] = false,
-        ["allow_ownship_export"] = true,
-        ["allow_object_export"] = false,
-        ["pause_on_load"] = false,
-        ["allow_sensor_export"] = false,
-        ["event_Takeoff"] = true,
-        ["pause_without_clients"] = true,
-        ["client_outbound_limit"] = 0,
-        ["client_inbound_limit"] = 0,
-        ["server_can_screenshot"] = true,
-        ["voice_chat_server"] = false,
-        ["allow_change_skin"] = true,
-        ["event_Connect"] = true,
-        ["allow_trial_only_clients"] = false,
-        ["event_Kill"] = true,
-        ["event_Crash"] = true,
-        ["event_Ejecting"] = true,
-        ["maxPing"] = 0,
-        ["event_Role"] = true,
-        ["resume_mode"] = 2,
-    }, -- end of ["advanced"]
-    ["port"] = 10310,
-    ["mode"] = 0,
-    ["bind_address"] = "",
-    ["isPublic"] = true,
-    ["uri"] = "startServer",
-    ["password"] = "",
-    ["listShuffle"] = false,
-    ["name"] = "[4YA] TRAINING 24/7 MARIANAS PVE BETA US SERVER",
-    ["listLoop"] = true,
-    ["missionList"] = 
-    {
-        [1] = "C:\\Users\\Administrator\\Saved Games\\BetaServer3\\Missions\\4YA_Marianas_LS_V.1.7.miz",
-    }, -- end of ["missionList"]
-    ["require_pure_clients"] = false,
-    ["require_pure_models"] = true,
-    ["maxPlayers"] = "25",
-} -- end of cfg
+cfg = {
+	['advanced'] = {
+		['allow_change_skin'] = true,
+		['allow_change_tailno'] = true,
+		['allow_object_export'] = false,
+		['allow_ownship_export'] = true,
+		['allow_sensor_export'] = false,
+		['allow_trial_only_clients'] = false,
+		['client_inbound_limit'] = 0,
+		['client_outbound_limit'] = 0,
+		['disable_events'] = false,
+		['event_Connect'] = true,
+		['event_Crash'] = true,
+		['event_Ejecting'] = true,
+		['event_Kill'] = true,
+		['event_Role'] = true,
+		['event_Takeoff'] = true,
+		['maxPing'] = 0,
+		['pause_on_load'] = false,
+		['pause_without_clients'] = true,
+		['resume_mode'] = 2,
+		['server_can_screenshot'] = true,
+		['voice_chat_server'] = false,
+	},
+	['bind_address'] = '',
+	['description'] = '[4YA] TRAINING 24/7 MARIANAS PVE BETA US SERVER\n\n**DISCORD**  https://discord.gg/CaYRRDd\n**SRS**      Type  -motd  in-game chat for ip\n\nWelcome to 4YA Training Grounds mission, this mission is designed to help both new and experienced pilots refine \ntheir skills in their chosen airframe. The mission incorporates both fixed wing and rotary wing aircraft and the \nability to train on both Air to Air/Air to Ground techniques, missile evasion, land-based circuit landings as \nwell as carrier-based recovery operations. You may fly alone or with a wingman/squadron to develop skills essential in a coordinated attack.\n\nAvailable:\n\nCtld\nCombined Arms\nPVE\nSRS\nCarrierOps ',
+	['isPublic'] = true,
+	['listLoop'] = true,
+	['listShuffle'] = false,
+	['listStartIndex'] = 1,
+	['maxPlayers'] = '25',
+	['missionList'] = {
+		[1] = 'C:/Users/Administrator/Saved Games/BetaServer3/Missions/4YA_Marianas_LS_V.1.15.miz',
+	},
+	['mode'] = 0,
+	['name'] = '[4YA] TRAINING 24/7 MARIANAS PVE BETA US SERVER',
+	['password'] = '',
+	['port'] = 10310,
+	['require_pure_clients'] = false,
+	['require_pure_models'] = true,
+	['require_pure_textures'] = false,
+	['uri'] = 'startServer',
+}
