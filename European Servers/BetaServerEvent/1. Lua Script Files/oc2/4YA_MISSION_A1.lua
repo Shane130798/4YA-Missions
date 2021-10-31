da0ad5ece1fac5242e2eac7d31e1d77aff70ca89
jayirving21 <86188742+jayirving21@users.noreply.github.com>
2021-10-31 17:12:28 +0000

    Update 4YA_MISSION_A1.lua

--- European Servers/BetaServerEvent/1. Lua Script Files/oc2/4YA_MISSION_A1.lua
+++ European Servers/BetaServerEvent/1. Lua Script Files/oc2/4YA_MISSION_A1.lua
@@ -1,32 +1,32 @@
 USAF_1 = SPAWN:New( "USAF_1" )
 :InitRandomizePosition(true,0,0)
 	:InitLimit( 2, 500 )
-	:SpawnScheduled( 50, .850 )
+	:SpawnScheduled( 300, .850 )
  
 USAF_2 = SPAWN:New( "USAF_2" )
 :InitRandomizePosition(true,0,0)
 	:InitLimit( 2, 500 )
-	:SpawnScheduled( 50, .850 )
+	:SpawnScheduled( 300, .850 )
  
 USAF_3 = SPAWN:New( "USAF_3" ) 
 :InitRandomizePosition(true,0,0)
 	:InitLimit( 2, 500 )
-	:SpawnScheduled( 50, .850 )
+	:SpawnScheduled( 300, .850 )
  
 USAF_4 = SPAWN:New( "USAF_4" )
 :InitRandomizePosition(true,0,0)
 	:InitLimit( 2, 500 )
-	:SpawnScheduled( 100, .850 )
+	:SpawnScheduled( 420, .850 )
 
 USAF_5 = SPAWN:New( "USAF_5" ) 
 :InitRandomizePosition(true,0,0)
 	:InitLimit( 2, 500 )
-	:SpawnScheduled( 100, .850 )
+	:SpawnScheduled( 420, .850 )
  
 USAF_6 = SPAWN:New( "USAF_6" )
 :InitRandomizePosition(true,0,0)
 	:InitLimit( 2, 500 )
-	:SpawnScheduled( 100, .850 )
+	:SpawnScheduled( 420, .850 )
 
 CONVOY_7 = SPAWN:New( "CONVOY_7" )
 :InitRandomizePosition(true,0,0)
